7f704a996baaf7d7dff39efeb5d07c00aa1151f8
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-02 09:50:38 +0900

    feat: 데이터 받는 코드

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-console.log('hello log')
+// console.log('hello log')
 
 const express = require('express')
-const app = express()
+const app = express();
+
+
+const multer = require('multer')
 
 app.get('/', (req, res) => {
     console.log('hello log')
-    res.send('Hello, World!') 
+    res.send('Hello, World!')
+});
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    res.send('Image received');
 });
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000')
-})
+    console.log('Server started on port 3000');
+});
