deda4c7f1d63fc1686ff3425d93a719418db0fbf
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-02 13:12:00 +0900

    feat, chore: 메인 라우터 코드 이전, 샘플 코드 index.js에 통합

--- index.js
+++ index.js
@@ -3,29 +3,11 @@
 const express = require('express')
 const app = express();
 
+const mainRouter = require('./routes/main');
+const sampleRouter = require('./routes/example');
 
-const multer = require('multer')
-
-app.get('/', (req, res) => {
-    console.log('hello log')
-    res.send('Hello, World!')
-});
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    },
-});
-
-const upload = multer({ storage: storage });
-
-app.post('/upload', upload.single('image'), (req, res) => {
-    console.log(req.file);
-    res.send('Image received');
-});
+app.use('/', mainRouter);
+app.use('/example', sampleRouter);
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
---
+++ routes/main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer')
+
+router.get('/', (req, res) => {
+    console.log('hello log')
+    res.send('Hello, World!')
+});
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    res.send('Image received');
+});
+
+module.exports = router;
