d545c77b4e9e8bcd5c177e86821b97f198d07aa9
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-23 17:04:13 +0900

    0.1.1 feat: 웹캠 이미지 수신 API 작성, AI 처리 코드로 이미지 데이터 전달

--- routes/socket_events.js
+++ routes/socket_events.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('test');
 });
 
+// body-parser 설정
+// body parser를 사용해 application/json 파싱 (기본 100kb -> 10mb 제한)
+router.use(bodyParser.json({ limit: '10mb' }));
+// body parser를 사용해 application/x-www-form-urlencoded 파싱 (기본 100kb -> 10mb 제한)
+router.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 // 웹소켓을 정의한 코드
 const wss = require('../socket/websocket');
@@ -25,6 +31,22 @@ router.get('/test_py', async (req, res) => {
             client.send('server send 웹캠 이미지 전달!');
         }
     });
+});
+
+router.post('/data', async (req, res) => {
+    const data = req.body;
+    console.log(data); // { index: 0, ret: true, frame: [too large]}
+
+
+    // // Send a WebSocket message to all connected clients
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(data));
+            // client.send(data);
+        }
+    });
+
+    res.sendStatus(200);
 });
 
 module.exports = router;
