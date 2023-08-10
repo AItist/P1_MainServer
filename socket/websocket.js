1ff8a4d13f0ba3b6fd7983635ed845da92119fd1
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-08-10 15:05:59 +0900

    0.1.4 feat: 포트 설정 변경

--- socket/websocket.js
+++ socket/websocket.js
@@ -1,25 +1,27 @@
 const WebSocket = require('ws');
 
-const cam_ai_port = 8070;
-const python_port = 8080;
-const unity_om_port = 8081;
-const unity_main_port = 8082;
+const p1_cam_ai_port = 8070;
+const p2_python_pose_merge_port = 8071;
+const p3_unity_obj_maker_port = 8072;
+const p4_python_img_merge_port = 8073;
+const p5_unity_main_port = 8074;
 
-const wss_cam_ai = new WebSocket.Server({ port: cam_ai_port });
-const wss_python = new WebSocket.Server({ port: python_port });
-const wss_unity_object_maker = new WebSocket.Server({ port: unity_om_port });
-const wss_unity_main = new WebSocket.Server({ port: unity_main_port });
+const wss1_cam_ai = new WebSocket.Server({ port: p1_cam_ai_port });
+const wss2_python_pose_merge = new WebSocket.Server({ port: p2_python_pose_merge_port });
+const wss3_unity_object_maker = new WebSocket.Server({ port: p3_unity_obj_maker_port });
+const wss4_python_img_merge = new WebSocket.Server({ port: p4_python_img_merge_port });
+const wss5_unity_main = new WebSocket.Server({ port: p5_unity_main_port });
 
 let packet = {};
 
 // 11 : webcam 이미지를 받는 웹소켓 연결시
-wss_cam_ai.on('connection', (ws, req) => {
+wss1_cam_ai.on('connection', (ws, req) => {
 
     const ip = req.headers;
     console.log('Cam AI WebSocket client connected', ip['host']);
 
     // Send a welcome message to the client
-    ws.send('Welcome to the WebSocket server! ', cam_ai_port);
+    ws.send('Welcome to the WebSocket server! ', p1_cam_ai_port);
     ws.on('message', (message) => {
         // 클라에서 데이터 전달받음
         // const _time = new Date();
@@ -64,7 +66,7 @@ wss_cam_ai.on('connection', (ws, req) => {
             const chunkSize = 1000; // 글자수 n 단위로 분할
 
             packet['indexes'].sort((a, b) => a - b);
-            wss_python.clients.forEach(function each(client) {
+            wss2_python_pose_merge.clients.forEach(function each(client) {
                 if (client.readyState === WebSocket.OPEN) {
                     for (let i = 0; i < packet_data.length; i += chunkSize) {
                         const chunk = packet_data.slice(i, i + chunkSize);
@@ -101,20 +103,20 @@ wss_cam_ai.on('connection', (ws, req) => {
 
 
 // 22 : AI 가공 웹소켓 연결시
-wss_python.on('connection', (ws, req) => {
+wss2_python_pose_merge.on('connection', (ws, req) => {
 
     const ip = req.headers;
     console.log('Python Websocket Client connected', ip['host']);
 
     // Send a welcome message to the client
-    ws.send('Welcome to the ai WebSocket server! ', python_port);
+    ws.send('Welcome to the ai WebSocket server! ', p2_python_pose_merge_port);
     ws.on('message', (message) => {
         // 클라에서 데이터 전달받음
         // const _time = new Date();
         // console.log(`ai >> object-maker [${_time}]`);
 
         // 1. 받은 이미지를 바로 유니티와 연결된 웹소켓으로 전달한다.
-        wss_unity_object_maker.clients.forEach(function each(client) {
+        wss3_unity_object_maker.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
             }
@@ -129,13 +131,13 @@ wss_python.on('connection', (ws, req) => {
 });
 
 // 33 : 객체 생성 Unity 웹소켓 연결시
-wss_unity_object_maker.on('connection', (ws, req) => {
+wss3_unity_object_maker.on('connection', (ws, req) => {
 
     const ip = req.headers;
     console.log('Unity Object-maker WebSocket client connected', ip['host']);
 
     // Send a welcome message to the client
-    ws.send('Welcome to the WebSocket server! ', unity_om_port);
+    ws.send('Welcome to the WebSocket server! ', p3_unity_obj_maker_port);
     ws.on('message', (message) => {
         // 클라에서 데이터 전달받음
         // const _time = new Date();
@@ -143,7 +145,37 @@ wss_unity_object_maker.on('connection', (ws, req) => {
 
         // TODO: 2차 메인 유니티 프로젝트로 전달
         // 1. 받은 이미지를 바로 유니티와 연결된 웹소켓으로 전달한다.
-        wss_unity_main.clients.forEach(function each(client) {
+        wss5_unity_main.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('error', (error) => {
+        console.error('error', error);
+    });
+    ws.on('close', () => {
+        console.log('Unity Object-maker Websocket Client disconnected ', ip['host']);
+    });
+});
+
+// _4 : 이미지 병합 python 프로그램과 연결
+wss4_python_img_merge.on('connection', (ws, req) => {
+
+    const ip = req.headers;
+    console.log('python image merger WebSocket client connected', ip['host']);
+
+    // Send a welcome message to the client
+    ws.send('Welcome to the WebSocket server! ', p4_python_img_merge_port);
+    ws.on('message', (message) => {
+        // 클라에서 데이터 전달받음
+        // const _time = new Date();
+        // console.log(`object-maker >> main [${_time}]`);
+
+        // TODO: 2차 메인 유니티 프로젝트로 전달
+        // 1. 받은 이미지를 바로 유니티와 연결된 웹소켓으로 전달한다.
+        wss5_unity_main.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
             }
@@ -159,13 +191,13 @@ wss_unity_object_maker.on('connection', (ws, req) => {
 });
 
 // 메인 Unity 웹소켓 연결시
-wss_unity_main.on('connection', (ws, req) => {
+wss5_unity_main.on('connection', (ws, req) => {
 
     const ip = req.headers;
     console.log('Unity Main WebSocket client connected', ip['host']);
 
     // Send a welcome message to the client
-    ws.send('Welcome to the WebSocket server! ', unity_main_port);
+    ws.send('Welcome to the WebSocket server! ', p5_unity_main_port);
     ws.on('message', (message) => {
         // 클라에서 데이터 전달받음
         console.log('message', message.toString());
@@ -179,4 +211,4 @@ wss_unity_main.on('connection', (ws, req) => {
     });
 });
 
-module.exports = wss_python;
+module.exports = wss2_python_pose_merge;
